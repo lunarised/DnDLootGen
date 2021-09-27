9e785c81b6665524f0c529e3749a280dba7db8c7
lunarised <lunarised@outlook.com>
2021-09-28 10:18:41 +1300

    prevent collisions during add

--- ts/add.ts
+++ ts/add.ts
@@ -1,10 +1,29 @@
 export const addLoot = (name: String, val: Value): void => {
-const fs = require("fs");
-let lootJSON = fs.readFileSync("./store/loot.json", "utf-8");
+	const fs = require("fs");
+	let lootJSON = fs.readFileSync("./store/loot.json", "utf-8");
 
-let lootList = JSON.parse(lootJSON);
-lootList.push({name: name, gold: val.gold, silver: val.silver, copper: val.copper});
-lootJSON = JSON.stringify(lootList);
-fs.writeFileSync("./store/loot.json", lootJSON,"utf-8" );
+	let lootList: Item[] = JSON.parse(lootJSON);
+	let collision = false;
+	lootList.forEach((item) => {
+		if (item.name == name) {
+			collision = true;
+		}
+	});
+	if (!collision) {
+		lootList.push({
+			name: name,
+			gold: val.gold,
+			silver: val.silver,
+			copper: val.copper,
+		});
 
-}
+		lootJSON = JSON.stringify(lootList);
+		fs.writeFileSync("./store/loot.json", lootJSON, "utf-8");
+	} else {
+		console.log(
+			"There is already an item called ",
+			name,
+			", please rename your item"
+		);
+	}
+};
